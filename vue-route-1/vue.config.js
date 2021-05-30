9b850f66f01cfe684e068d9b2d9b907d5f14fca1
ana cossio <anacossio7@gmail.com>
2021-05-29 23:21:13 -0300

    prueba

---
+++ vue-route-1/vue.config.js
@@ -0,0 +1,15 @@
+// vue.config.js
+
+// /**
+//  * @type {import('@vue/cli-service').ProjectOptions}
+//  */
+//  module.exports = {
+//   // options...
+// }
+
+module.exports = {
+  publicPath: '/practice-frontend/vue-route-1/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/practice-frontend/vue-route-1/'
+  //   : '/'
+}
